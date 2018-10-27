dc0172188daa79b77085d1c7233fdf3c7289239b
Shekhar Gupta <shekhargupta677@gmail.com>
2018-10-27 12:36:53 +0530

    done with products route

---
+++ api/routes/products.js
@@ -0,0 +1,149 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
+
+router.get('/', (req, res, next) => {
+  Product.find()
+    .select('_id name price')
+    .exec()
+    .then(doc => {
+      const response = {
+        count: doc.length,
+        products: doc.map(doc => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            _id: doc._id,
+            request: {
+              type: 'GET',
+              url: 'http://localhost:3000/products/'+ doc._id
+            }
+          }
+        })
+      };
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      })
+    });
+});
+
+router.post('/', (req, res, net) => {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price
+  });
+
+  product.save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: 'Product Created',
+        createdProduct: {
+          name: result.name,
+          price: result.price,
+          _id: result.id,
+          request: {
+            type: 'GET',
+            url: 'http://localhost:3000/products/'+ result._id
+          }
+        }
+      })
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      })
+    });
+});
+
+router.get('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+  Product.findById(id)
+    .select('_id name price')
+    .exec()
+    .then(doc => {
+      console.log(doc);
+      if(doc) {
+        res.status(200).json({
+          product: doc,
+          request: {
+            type: 'GET',
+            url: 'http://localhost:3000/products'
+          }
+        });
+      } else {
+        res.status(404).json({
+          message: 'No valid entry found'
+        });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+// PATCH request should be an array of objects
+// example:
+// [
+// 	{ "propName": "name", "value": "Huawei P20 pro" },
+// 	{ "propName": "price", "value": "33000" }
+// ]
+router.patch('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: 'Product updated',
+        request: {
+          type: 'GET',
+          url: 'http://localhost:3000/products/'+ id
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      })
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+  Product.remove({ _id: id })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: 'Product deleted',
+        request: {
+          type: 'POST',
+          url: 'http://localhost:3000/products',
+          body: { name: 'String', price: 'Number' }
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+const productRoutes = require('./api/routes/products');
+const ordersRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb://localhost/rest_api', { useNewUrlParser: true });
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use('/products', productRoutes);
+app.use('/orders', ordersRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  })
+});
+
+module.exports = app;
